5f724ae0754755c1064500ceb19af54ba532deda
Sergey Y <s@rapidsd.com>
2025-04-25 02:22:01 +1000

    Added landing page for unauthorized users
    Signed-off-by: Sergey Y <s@rapidsd.com>

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -1,25 +1,29 @@
-'use client';
-
-import { useState } from 'react';
-import Sidebar from '@/components/Sidebar';
-import TabView from '@/components/TabView';
+export const runtime = 'edge';
+import { cookies } from 'next/headers';
+import HomeClient from './page.client';
 import NoticeBar from '@/components/NoticeBar';
+import Link from 'next/link';
 
-export default function Home() {
-  // selectedService will be used to update UI based on service selection
-  const [selectedService, setSelectedService] = useState<string>('');
-
-  return (
-    <div className="flex flex-col h-screen bg-gray-900">
-      <NoticeBar />
-      
-      <div className="flex flex-1 min-h-0 relative">
-        <Sidebar onServiceSelect={setSelectedService} />
-        
-        <main className="flex-1 min-h-0">
-          <TabView selectedService={selectedService} />
-        </main>
+export default async function Home() {
+  const cookieStore = await cookies();
+  const refreshCookie = cookieStore.get('refresh_token');
+  if (!refreshCookie) {
+    return (
+      <div className="flex flex-col min-h-screen bg-gray-900 text-white p-6">
+        <NoticeBar />
+        <div className="flex flex-col items-center justify-center flex-1 space-y-6">
+          <h1 className="text-3xl font-bold">Welcome to Cloudflare Studio</h1>
+          <div className="flex space-x-4">
+            <Link href="/about">
+              <button className="px-4 py-2 bg-blue-500 rounded hover:bg-blue-600">About</button>
+            </Link>
+            <a href="https://auth.cloudflare.studio/login">
+              <button className="px-4 py-2 bg-green-500 rounded hover:bg-green-600">Login</button>
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return <HomeClient />;
 }
